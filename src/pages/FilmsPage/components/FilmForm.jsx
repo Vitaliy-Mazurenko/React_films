1df8a77c71b22a043ef385f22deaf096592b2e08
romannikiforov <escaro@ukr.net>
2021-10-15 19:34:11 +0300

    before task

--- src/pages/FilmsPage/components/FilmForm.jsx
+++ src/pages/FilmsPage/components/FilmForm.jsx
@@ -2,6 +2,12 @@ import { Component } from "react";
 
 const inititialData = {
   title: "",
+  img: "",
+  description: "",
+  director: "",
+  price: "",
+  duration: "",
+  featured: false,
 };
 
 class FilmForm extends Component {
@@ -9,7 +15,10 @@ class FilmForm extends Component {
     data: inititialData,
   };
 
-  handleStringChange = (e) => {};
+  handleStringChange = (e) =>
+    this.setState({
+      data: { ...this.state.data, [e.target.name]: e.target.value },
+    });
 
   handleCheckboxChange = (e) => {};
 
@@ -21,24 +30,125 @@ class FilmForm extends Component {
   };
 
   render() {
+    const { data } = this.state;
     return (
-      <form className="ui form">
+      <form onSubmit={this.handleSubmit} className="ui form">
         <div className="ui grid mb-3">
-          {/* left  column  START */}
-          <div className="ten wide column">
-            {/* title START */}
-            <div className="field">
-              <label htmlFor="title">Film title</label>
-              <input
-                type="text"
-                name="title"
-                id="title"
-                placeholder="film title"
+          {/* two column row START */}
+          <div className="two column row">
+            {/* left  column  START */}
+            <div className="ten wide column">
+              {/* title START */}
+              <div className="field">
+                <label htmlFor="title">Film title</label>
+                <input
+                  value={data.title}
+                  onChange={this.handleStringChange}
+                  type="text"
+                  name="title"
+                  id="title"
+                  placeholder="film title"
+                />
+              </div>
+              {/* title END */}
+              {/* image field START */}
+              <div className="field img-grid">
+                <label htmlFor="img">Image</label>
+                <input
+                  value={data.img}
+                  onChange={this.handleStringChange}
+                  name="img"
+                  id="img"
+                />
+
+                <div className="inp-file">
+                  <label htmlFor="photo">Photo</label>
+                  <input type="file" id="photo" />
+                </div>
+              </div>
+              {/* image field END */}
+              {/* description field START */}
+              <div className="column row field">
+                <label htmlFor="description">Film description</label>
+                <textarea
+                  value={data.description}
+                  onChange={this.handleStringChange}
+                  name="description"
+                  id="description"
+                  placeholder="film description"
+                ></textarea>
+              </div>
+              {/* description field END */}
+            </div>
+            {/* left  column  END */}
+            {/*  right column START */}
+            <div className="six wide column">
+              <img
+                src="http://via.placeholder.com/250x250"
+                className="ui image imgfit"
+                alt="myimg"
               />
             </div>
-            {/* title END */}
+            {/*  right column END */}
           </div>
-          {/* left  column  END */}
+          {/* two column row END */}
+
+          {/* three column row START */}
+          <div className="three column row">
+            {/* director START */}
+            <div className="column field">
+              <label htmlFor="director">Director</label>
+              <input
+                value={data.director}
+                onChange={this.handleStringChange}
+                type="text"
+                name="director"
+                id="director"
+                placeholder="film director"
+              />
+            </div>
+            {/* director END*/}
+            {/* duration START */}
+            <div className="column field">
+              <label htmlFor="duration">Duration</label>
+              <input
+                value={data.duration}
+                onChange={this.handleNumberChange}
+                type="number"
+                name="duration"
+                id="duration"
+                placeholder="Duration"
+              />
+            </div>
+            {/* duration END */}
+            {/* price START */}
+            <div className="column field">
+              <label htmlFor="price">Price</label>
+              <input
+                value={data.price}
+                onChange={this.handleNumberChange}
+                type="number"
+                name="price"
+                id="price"
+                placeholder="price"
+              />
+            </div>
+            {/* price END */}
+          </div>
+          {/* three column row END */}
+
+          {/* featured START */}
+          <div className="six wide column inline field">
+            <label htmlFor="featured">Featured</label>
+            <input
+              checked={data.featured}
+              onChange={this.handleCheckboxChange}
+              type="checkbox"
+              name="featured"
+              id="featured"
+            />
+          </div>
+          {/* featured END */}
 
           {/* Buttons START */}
           <div className="ui fluid buttons">
